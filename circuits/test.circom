1844f4b6128dc5d9c8704d4a08be5dd4201a1579
Manuel Sanchez <manuelsfdelavega@gmail.com>
2024-11-04 22:50:08 -0300

    separate input

---
+++ circuits/test.circom
@@ -0,0 +1,69 @@
+pragma circom 2.0.0;
+
+include "./node_modules/circomlib/circuits/smt/smtprocessor.circom";
+
+template Test(nLevels) {
+    signal input oldRoot;
+    signal input newRoot;
+    signal input siblings[nLevels];
+    signal input isOld0;
+
+    signal input oldId;
+    signal input oldDoctorId;
+    signal input oldPresentationId;
+    signal input oldPatientId;
+    signal input oldQuantity;
+    signal input oldEmitedAt;
+    signal input oldKey;
+
+    signal input newId;
+    signal input newDoctorId;
+    signal input newPresentationId;
+    signal input newPatientId;
+    signal input newQuantity;
+    signal input newEmitedAt;
+    signal input newKey;
+
+    signal input fnc; // 0: insert, 1: mark as used
+    fnc * (1 - fnc) === 0; // fnc should be 0 or 1
+
+    component processor = SMTProcessor(nLevels);
+    
+    component oldPoseidon = Poseidon(7);
+    component newPoseidon = Poseidon(7);
+
+    oldPoseidon.inputs[0] <== oldId;
+    oldPoseidon.inputs[1] <== oldDoctorId;
+    oldPoseidon.inputs[2] <== oldPresentationId;
+    oldPoseidon.inputs[3] <== oldPatientId;
+    oldPoseidon.inputs[4] <== oldQuantity;
+    oldPoseidon.inputs[5] <== oldEmitedAt;
+    oldPoseidon.inputs[6] <== 0;
+
+    newPoseidon.inputs[0] <== newId;
+    newPoseidon.inputs[1] <== newDoctorId;
+    newPoseidon.inputs[2] <== newPresentationId;
+    newPoseidon.inputs[3] <== newPatientId;
+    newPoseidon.inputs[4] <== newQuantity;
+    newPoseidon.inputs[5] <== newEmitedAt;
+    newPoseidon.inputs[6] <== fnc;
+
+    processor.oldRoot <== oldRoot;
+    processor.siblings <== siblings;
+    processor.oldKey <== oldKey;
+    processor.oldValue <== oldPoseidon.out;
+    processor.isOld0 <== isOld0;
+    processor.newKey <== newKey;
+    processor.newValue <== newPoseidon.out;
+    processor.fnc <== [1 - fnc, fnc]; // [1, 0] => insert, [0, 1] => update
+
+    processor.newRoot === newRoot;
+    (oldId - newId) * (1 - isOld0) === 0;
+    (oldDoctorId - newDoctorId) * (1 - isOld0) === 0;
+    (oldPatientId - newPatientId) * (1 - isOld0) === 0;
+    (oldPresentationId - newPresentationId) * (1 - isOld0) === 0;
+    (oldQuantity - newQuantity) * (1 - isOld0) === 0;
+    (oldEmitedAt - newEmitedAt) * (1 - isOld0) === 0;
+}
+
+component main {public [oldRoot, newRoot]} = Test(4);
---
+++ src/aux/update_test3.ts
@@ -0,0 +1,144 @@
+import { newMemEmptyTrie } from "circomlibjs";
+import { poseidon6 } from "poseidon-lite";
+import * as snarkjs from "snarkjs";
+
+interface Proof {
+  pi_a: string[];
+  pi_b: string[][];
+  pi_c: string[];
+}
+
+const parseProof = (proof: Proof) => ({
+  pi_a: proof.pi_a.map((x) => "0x" + BigInt(x).toString(16)).slice(0, -1),
+  pi_b: proof.pi_b
+    .map((x) => x.map((y) => "0x" + BigInt(y).toString(16)).reverse())
+    .slice(0, -1),
+  pi_c: proof.pi_c.map((x) => "0x" + BigInt(x).toString(16)).slice(0, -1)
+});
+
+const hashPrescription = (prescription: {
+  id: number;
+  doctorId: number;
+  presentationId: number;
+  patientId: number;
+  quantity: number;
+  emitedAt: number;
+}) => {
+  return poseidon6([
+    prescription.id,
+    prescription.doctorId,
+    prescription.presentationId,
+    prescription.patientId,
+    prescription.quantity,
+    prescription.emitedAt
+  ]);
+};
+
+async function main() {
+  const tree = await newMemEmptyTrie();
+  const _key = tree.F.e(1);
+  const _value = {
+    id: 1,
+    doctorId: 1,
+    presentationId: 1,
+    patientId: 1,
+    quantity: 1,
+    emitedAt: 1
+  };
+
+  const key = tree.F.e(_key);
+  const value = hashPrescription(_value);
+
+  const res = await tree.insert(key, value);
+
+  let siblings = res.siblings;
+  for (let i = 0; i < siblings.length; i++)
+    siblings[i] = tree.F.toObject(siblings[i]);
+  while (siblings.length < 4) siblings.push(0);
+
+  await snarkjs.groth16.fullProve(
+    {
+      fnc: 0,
+      oldRoot: tree.F.toObject(res.oldRoot),
+      newRoot: tree.F.toObject(res.newRoot),
+      siblings: siblings,
+      oldKey: 0,
+      oldId: 0,
+      oldDoctorId: 0,
+      oldPresentationId: 0,
+      oldPatientId: 0,
+      oldQuantity: 0,
+      oldEmitedAt: 0,
+      newId: _value.id,
+      newDoctorId: _value.doctorId,
+      newPresentationId: _value.presentationId,
+      newPatientId: _value.patientId,
+      newQuantity: _value.quantity,
+      newEmitedAt: _value.emitedAt,
+      isOld0: 1,
+      newKey: tree.F.toObject(key)
+    },
+    "build/test/test_js/test.wasm",
+    "build/test/circuit_final.zkey"
+  );
+
+  // --------------------------------
+
+  const _updatedKey = tree.F.e(_key);
+  const _updatedValue = {
+    id: 1,
+    doctorId: 1,
+    presentationId: 1,
+    patientId: 25,  // changed
+    quantity: 1,
+    emitedAt: 1
+  };
+
+  const updatedValue = hashPrescription(_updatedValue);
+
+  const updatedRes = await tree.update(key, updatedValue);
+  siblings = updatedRes.siblings;
+  for (let i = 0; i < siblings.length; i++)
+    siblings[i] = tree.F.toObject(siblings[i]);
+  while (siblings.length < 4) siblings.push(0);
+
+  const { proof, publicSignals } = await snarkjs.groth16.fullProve(
+    {
+      fnc: 1,
+      oldRoot: tree.F.toObject(updatedRes.oldRoot),
+      newRoot: tree.F.toObject(updatedRes.newRoot),
+      siblings: siblings,
+      oldKey: tree.F.toObject(key),
+      oldId: _value.id,
+      oldDoctorId: _value.doctorId,
+      oldPresentationId: _value.presentationId,
+      oldPatientId: _value.patientId,
+      oldQuantity: _value.quantity,
+      oldEmitedAt: _value.emitedAt,
+      newId: _updatedValue.id,
+      newDoctorId: _updatedValue.doctorId,
+      newPresentationId: _updatedValue.presentationId,
+      newPatientId: _updatedValue.patientId,
+      newQuantity: _updatedValue.quantity,
+      newEmitedAt: _updatedValue.emitedAt,
+      isOld0: 0,
+      newKey: tree.F.toObject(key)
+    },
+    "build/test/test_js/test.wasm",
+    "build/test/circuit_final.zkey"
+  );
+
+  console.log("Proof: ", proof);
+  console.log("Proof: ", parseProof(proof));
+  console.log(
+    "Public signals: ",
+    publicSignals.map((x) => BigInt(x).toString(16))
+  );
+  console.log("Root hex: ", BigInt(tree.F.toString(res.newRoot)).toString(16));
+  console.log(
+    "Old Root hex: ",
+    BigInt(tree.F.toString(res.oldRoot)).toString(16)
+  );
+}
+
+main();
